b8d6f79c53522fae6181e9f1aa539119e8e11b26
Lynda Benabdessadok <lylyben74@gmail.com>
2023-06-14 16:59:28 +0200

    add local storage date

--- src/components/progressionBar.js
+++ src/components/progressionBar.js
@@ -1,9 +1,33 @@
 import { Fragment } from "react";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import "../App.css";
+import { hover } from "@testing-library/user-event/dist/hover";
 
 
 function ProgressionBar() {
+
+        const [progression, setProgression] = useState(0);
+        const [date, setDate] = useState('');
+        const currentDate = new Date();
+
+
+
+        const increaseProgression = () => {
+            if ( progression < 100) {
+
+              setProgression(progression + 18); 
+              localStorage.setItem('date', currentDate.toString());
+              setDate(currentDate.toString()); 
+                
+            }else if (progression > 100 ) {
+
+              setProgression(0);
+
+        
+            }
+          };
 
 
 
@@ -12,16 +36,40 @@ function ProgressionBar() {
                 <div className="progressionBar">
                 <span> Collaboration en discussion - POC </span>
 
-                <div className="progression">
+                <div className="progression" style={{ width: `${progression}%`, backgroundColor : '#006dff' }}>
                     <div className="points">
 
-                        <button className="point"></button>
-                        <button className="point"></button>
-                        <button className="point"></button>
-                        <button className="point"></button>
-                        <button className="point"></button>
-                        <button className="point"></button>
-                        <button className="point"></button>
+                        <div onMouseOver={() => console.log(localStorage.getItem('date'))}>
+                        <button className="point" onClick={increaseProgression}></button>                        
+                        </div> 
+                                               
+                        <div onMouseOver={() => console.log(localStorage.getItem('date'))}>
+                        <button className="point" onClick={increaseProgression}></button>                        
+                        </div>
+
+                        <div onMouseOver={() => console.log(localStorage.getItem('date'))}>
+                        <button className="point" onClick={increaseProgression}></button>                        
+                        </div>
+
+                        <div onMouseOver={() => console.log(localStorage.getItem('date'))}>
+                        <button className="point" onClick={increaseProgression}></button>                        
+                        </div>
+
+                        <div onMouseOver={() => console.log(localStorage.getItem('date'))}>
+                        <button className="point" onClick={increaseProgression}></button>                        
+                        </div>
+
+                        <div onMouseOver={() => console.log(localStorage.getItem('date'))}>
+                        <button className="point" onClick={increaseProgression}></button>                        
+                        </div>
+
+                        <div onMouseOver={() => console.log(localStorage.getItem('date'))}>
+                        <button className="point" onClick={increaseProgression}></button>                        
+                        </div>
+
+                        
+                        
+
                         
                     </div>
 
