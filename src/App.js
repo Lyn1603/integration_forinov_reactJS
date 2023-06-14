994084541db0a0310a05f9be9ed9f076dc310c70
Lynda Benabdessadok <lylyben74@gmail.com>
2023-06-14 11:45:24 +0200

    middle of the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import { Fragment } from 'react';
+import pdf from './assets/pdf.png';
 import Logo from './components/logo.js';
+import Web from './assets/website.png';
+import facebook from './assets/facebook.png';
+import linkedin from './assets/linkedin.png';
+import twitter from './assets/twitter.png';
+import combineshape from './assets/combinedShape.png';
 import LocalMap from './assets/map-localisation-copy.png'
-import Button1 from './components/button1.js';
+
 
 function App() {
   return (
@@ -22,8 +28,10 @@ function App() {
             <div className="photoprofile">
 
                   <Logo />
-                <Button1 />
 
+                  <button className="button1">
+                    <img src={pdf} alt="pdf" className="pdf" />
+                  </button>
             </div>
 
           <div className="about">
@@ -47,23 +55,53 @@ function App() {
 
         <div className="contact">
 
-          <div className="about">
 
           <img src={LocalMap} alt="map" className="map" />
-          
-              <div className="dots">
 
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-          
-              </div>
-           
+          <p className='adress'>Paris, FR</p>
+              
+          <img src={Web} alt="website" className="website" />
 
+          <p className='link'>www.demo.com</p>
 
-            </div>
+          <div className="social">
+
+            <img src={facebook} alt="social"  />
+            <img src={twitter} alt="social" />
+            <img src={linkedin} alt="social"  />
+            <img src={combineshape} alt="social" />
+
+          </div>
+        
+        </div>
+
+        <div className="text">
+
+          <span className='text1'>
+            Jubiwee est une application web qui mesure automatiquement le ressenti des collaborateurs, 
+          détecte les signaux faibles et populations à risque. 
+          Elle génère également des plans d’actions pour donner aux managers
+           les meilleures actions pour réduire le turnover, l’absentéisme et améliorer l’engagement dans leur équipe.
+          </span>
+        
         
         </div> 
+
+        <div className="button">
+
+        <button className="button2">
+                    <span style={{ color:"#006dff"}}>Ressources Humaines</span>
+        </button>
+
+          <div>
+          <button className="button3">
+                    <span style={{ color:"#006dff"}}>Software</span>
+          </button>
+          <button className="button4">
+                    <span style={{ color:"#006dff"}}>HR</span>
+          </button>
+          </div>
+        </div>
 
 
         </div>
